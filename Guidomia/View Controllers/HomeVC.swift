03c34ecb891b51e128fe5f8fdf8133ac1d6fbbda
iOSDevKamal <kamaltrapasiya@gmail.com>
2023-01-19 10:57:01 -0500

    Add functionality to read data from JSON file and save to local database

--- Guidomia/View Controllers/HomeVC.swift
+++ Guidomia/View Controllers/HomeVC.swift
@@ -6,24 +6,18 @@
 //
 
 import UIKit
+import Combine
 
 class HomeVC: UIViewController {
+    
+    private var cancellable: AnyCancellable?
+    let carViewModel = CarViewModel()
+    var carArr = [Car]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        cancellable = carViewModel.$carArr.sink(receiveValue: { items in
+            self.carArr = items
+        })
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Guidomia/View Model/CarViewModel.swift
@@ -0,0 +1,90 @@
+//
+//  CarViewModel.swift
+//  Guidomia
+//
+//  Created by Kamal Trapasiya on 2023-01-19.
+//
+
+import UIKit
+import CoreData
+
+class CarViewModel: NSObject, ObservableObject {
+    
+    @Published var carArr: [Car] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override init() {
+        super.init()
+        getData()
+    }
+    
+    func getData() {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Car")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request) as! [Car]
+            if !result.isEmpty {
+                carArr = result
+            }
+            else {
+                parseJSON()
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func parseJSON() {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: "car_list",
+                                                 ofType: "json"), let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                do {
+                    guard let jsonArr = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [[String:Any]] else { return }
+                    saveData(jsonArr: jsonArr)
+                }
+                catch {
+                    print(error)
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func saveData(jsonArr: [[String:Any]]) {
+        let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+        for dict in jsonArr {
+            let car = Car(entity: entity!, insertInto: context)
+            if let make = dict["make"] as? String {
+                car.make = make
+            }
+            if let model = dict["model"] as? String {
+                car.model = model
+            }
+            if let marketPrice = dict["marketPrice"] as? Double {
+                car.marketPrice = marketPrice
+            }
+            if let customerPrice = dict["customerPrice"] as? Double {
+                car.customerPrice = customerPrice
+            }
+            if let rating = dict["rating"] as? Int16 {
+                car.rating = rating
+            }
+            if let prosList = dict["prosList"] as? NSObject {
+                car.prosList = prosList
+            }
+            if let consList = dict["consList"] as? NSObject {
+                car.consList = consList
+            }
+        }
+        DispatchQueue.main.async {
+            do {
+                try self.context.save()
+                self.getData()
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+}
